6c8d562a7d9ccd641e408c703d1810f61e8d4a85
Taras <tstetsko@ClearlyAgileinc.com>
2020-11-16 15:53:34 -0500

    load blogPosts

--- test/association_test.js
+++ test/association_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -7,20 +8,21 @@ describe('Associations', () => {
     let joe, blogPost, comments;
     beforeEach((done) =>{
           joe = new User({name: 'Joe'});  
-          blogPost = new BlogPost ({title: 'JS is Greate', content: 'Yep it really is'});
+          blogPost = new BlogPost ({title: 'JS is Great', content: 'Yep it really is'});
           comment = new Comment({ content: 'Congrats on great post' });
           
           joe.blogPosts.push(blogPost);
-          blogPost.comments.push(comments);
+          blogPost.comments.push(comment);
           comment.user = joe;
          
           Promise.all([joe.save(), blogPost.save(), comment.save()]).then(() => done());
     });
 
-    it.only('save a relation berween a user and a blogpost', (done) => {
-       User.findOne({name: 'Joe'})
+    it('save a relation berween a user and a blogpost', (done) => {
+       User.findOne({name: 'Joe'}).populate('blogPosts')
        .then((user) => {
-           console.log(user);
+           console.log(user.blogPosts[0]);
+           assert(user.blogPosts[0].title === 'JS is Great');
            done();
        })
     })
